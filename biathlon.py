f2967392b953235ad6560d75897bf733780331bc
RoFo8072 <90692133+RoFo8072@users.noreply.github.com>
2021-09-30 11:55:36 +0200

    Update biathlon.py
    Fixed it some more

--- biathlon.py
+++ biathlon.py
@@ -69,11 +69,8 @@ while targets_to_string(ts) != ("0 0 0 0 0 "):
     x = input("Choose target: ")
     try:
         x = int(x)
-        if x > 4:
-            print("Invalid target")
-        else:    
-            shoot(x, ts)
-            view_targets(ts)  
-    except ValueError:
+        shoot(x, ts)
+        view_targets(ts)  
+    except (ValueError, IndexError):
         print("Invalid target")
 print("Game over, all targets hit") 
